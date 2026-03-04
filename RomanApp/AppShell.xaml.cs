b111a3ed5caa1fe3bcf3baca12e61c0e115b3e8a
romanj07 <romanjeff2005@gmail.com>
2026-03-04 14:57:57 +0100

    initial commit

---
+++ RomanApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace RomanApp;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ RomanApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace RomanApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
