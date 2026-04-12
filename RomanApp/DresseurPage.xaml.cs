9d4ac5b35f94beb2a3bded1bec0ec76e5db6e25e
romanj07 <romanjeff2005@gmail.com>
2026-04-12 20:41:51 +0200

    add the dresserpage

---
+++ RomanApp/DresseurPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using RomanApp.ViewModels;
+
+namespace RomanApp;
+
+public partial class DresseurPage : ContentPage
+{
+    private readonly DresseurViewModel _viewModel;
+    private bool _isInitialized;
+
+    
+    public DresseurPage(DresseurViewModel viewModel)
+    {
+        InitializeComponent();
+        _viewModel = viewModel;
+        BindingContext = _viewModel;
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        if (_isInitialized)
+        {
+            return;
+        }
+
+        _isInitialized = true;
+        await _viewModel.LoadSavedTeamCommand.ExecuteAsync(null);
+    }
+}
+
